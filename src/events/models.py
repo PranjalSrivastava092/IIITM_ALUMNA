ddab490de1a25e6fd7bffb7ab319e32497fe6409
Pranjal Srivastava <30670206+PranjalSrivastava092@users.noreply.github.com>
2017-12-15 16:14:22 +0530

    Fixed Indentation

--- src/events/models.py
+++ src/events/models.py
@@ -14,8 +14,8 @@ class Event(models.Model):
 
 
 	def __unicode__(self):
-        return self.title
+        	return self.title
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+        	return self.title
 
